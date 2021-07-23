bfb6ce2a421ef727ed8581f94c4a19fd759c54b0
Tran Bao <tranvanbao2201@gmail.com>
2021-07-23 12:49:10 +0700

    Show order history

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     base_title = t "page.base_title"
     page_title.blank? ? base_title : "#{page_title} | #{base_title}"
   end
+
+  def current_index page_index, page_size, index
+    (page_index - 1) * page_size + (index + 1)
+  end
 end
